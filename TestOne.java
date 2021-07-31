050b9735d5c613887322c675075855871cb55153
Aleksandr Kunevich <87695606+AleksandrKunevich@users.noreply.github.com>
2021-07-31 20:57:53 +0300

    Update TestOne.java
    Срфтпу

--- TestOne.java
+++ TestOne.java
@@ -3,5 +3,9 @@ public class TestOne{
   public static void main(String[] args){
     
     System.out.println("Test 1");
+    System.out.println("Test 1");
+    System.out.println("Test 1");
+    System.out.println("Test 1");
+    System.out.println("Test 1");
   }
 }
